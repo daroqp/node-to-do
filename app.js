4b7de40b8f8da1df308f8c48e4115cb02844be6d
Dario Quispe <daro.qp@gmail.com>
2021-04-18 23:37:01 -0300

    Menu Interface terminado

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 require('colors');
-const { inquirerMenu } = require('./helpers/inquirer');
+const { inquirerMenu, pausa } = require('./helpers/inquirer');
 
 console.clear();
 
-
 const main = async() => {
-
-	console.log('Hola Mundo');
 
 	let opt = '';
 	do {
 		
 		opt = await inquirerMenu();
 		console.log({ opt });
+
+		await pausa();
 
 	}while(opt !== '0');
 	
